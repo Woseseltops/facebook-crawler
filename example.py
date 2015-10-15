3741d38ad76903424c33b0719fc1e8fd276d7651
Wessel Stoop <wstoop@applejack.science.ru.nl>
2015-10-15 10:10:18 +0200

    Added example of preferred usage

---
+++ example.py
@@ -0,0 +1,14 @@
+from facebook_crawler import FacebookCrawler
+
+auth_user = 'olvandgame@gmail.com'
+password = open('passwords/'+auth_user).read().strip()
+crawler = FacebookCrawler(user,password)
+
+example_user = 'antal.vandenbosch'
+user = crawler.get_user(example_user)
+print(example_user + ' is '+user.age+' years old')
+
+for post in user.posts[0:10]:
+
+    print(post.date)
+    print(post.text)
